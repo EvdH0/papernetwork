10eafe08ab94ce2b606fdf731906676fadf3d886
EvdH0 <i@iric.nl>
2020-06-19 22:53:47 +0200

    Support for 3.6 3.7 and 3.8

--- setup.py
+++ setup.py
@@ -32,10 +32,8 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
