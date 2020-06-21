b7e5ee3480c0f80c7426af3c37be204464df4043
EvdH0 <i@iric.nl>
2020-06-21 17:13:26 +0200

    bump to 0.1.2

--- papernetwork/__init__.py
+++ papernetwork/__init__.py
@@ -1,6 +1,6 @@
 """paper-network - Collect and analyze scientific literature from Semantic Scholar"""
 
-__version__ = '0.1.0'
+__version__ = '0.1.2'
 __author__ = 'Eric van der Helm <i@iric.nl>'
 __all__ = []
 
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(filename):
 
 setup(
     name="papernetwork",
-    version="0.1.0",
+    version="0.1.2",
     url="https://github.com/EvdH0/papernetwork",
     license='MIT',
 
